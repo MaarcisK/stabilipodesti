74676cb0be59c0cdd2aceac23406e00a48655005
druvis <>
2022-01-19 14:20:57 +0000

    Tests

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 #Stabili podesti (Mārcis un Druvis 10.a) v2
+garums = float(input())
+platums = float(input())
+augstums = float(input())
 def materialuAprekins(garums, platums, augstums, skaits):
-  garums, platums, augstums = float
-  skaits = int
+  garums, platums, augstums = float(input())
+  skaits = int(input())
   return
-
-def materialaUzskaite(tips, izmers1, izmers2, skaits):
 
