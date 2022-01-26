d5cb8bb5417ab172770acb7539a08e37acfba1fe
Marcis <98029503+MaarcisK@users.noreply.github.com>
2022-01-26 13:28:26 +0000

    pabeigts v2

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def materialuAprekins(garums, platums, augstums, skaits):
   print("Ja vajadzīgi podesti, kas ir {:.1f} cm gari, {:.1f} cm plati un {:.1f} cm augsti un kopā jāpasūta {:.1f}.".format(garums, platums, augstums, skaits))
 
 materialuAprekins(garums ,platums, augstums, skaits)
-a = skaits * 6 #finieri
-b = skaits * 8 #stūri
-c = skaits * 12 #līstes
-print("Tad būs nepieciešamas " + str(a) + " finieru plāksnes, " + str(b) + " stūri un " + str(c) + " līstes.")
+x = skaits * 6 #finieri
+y = skaits * 8 #stūri
+z = skaits * 12 #līstes
+print("Tad būs nepieciešamas " + str(x) + " finieru plāksnes, " + str(y) + " stūri un " + str(z) + " līstes.")
